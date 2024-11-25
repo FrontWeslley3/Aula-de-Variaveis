a5fff2f7c7c8a0b7d334ef3c6ec11bd1de3679e9
WESLLEY MACHADO ELIAS <machadown93@gmail.com>
2024-11-25 17:07:33 -0300

    Aula de variavel

---
+++ src/Variaveis.java
@@ -0,0 +1,14 @@
+public class Variaveis {
+    public static void main(String[] args) {
+
+        String variavelA = "Weslley Estudando variavel ";
+        String variavelB = "Praticando Java Aula";
+        int variavelInt = 1;
+        double variavelDouble=1.2;
+
+        System.out.println(variavelA);
+        System.out.println(variavelB);
+        System.out.println(variavelInt);
+        System.out.println(variavelDouble);
+    }
+}
