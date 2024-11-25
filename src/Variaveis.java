cd81d080a630cc8703b9b10427d9dc2e7fe5eb28
WESLLEY MACHADO ELIAS <machadown93@gmail.com>
2024-11-25 18:13:53 -0300

    Scanner

--- src/Variaveis.java
+++ src/Variaveis.java
@@ -1,14 +1,21 @@
+// Variaveis e o nome da classe
+
 public class Variaveis {
+    //metodo responsavel para executar o programa
     public static void main(String[] args) {
+        //tipo de variavel
 
         String variavelA = "Weslley Estudando variavel ";
         String variavelB = "Praticando Java Aula";
         int variavelInt = 1;
         double variavelDouble=1.2;
 
+
+        //imprime a frase no console da aplicação
         System.out.println(variavelA);
         System.out.println(variavelB);
         System.out.println(variavelInt);
         System.out.println(variavelDouble);
+
     }
 }
